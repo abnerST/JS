fb29b3eb82cf255a915c7652b6600b9852718a5d
AbnerST <abnersouza287@outlook.com>
2024-08-30 14:40:55 -0300

    Usando filter() e Map()

---
+++ gerador/assets/js/main.js
@@ -0,0 +1,40 @@
+const pessoas = [
+    { nome: "Luiz", idade: 62 },
+    { nome: "Maria", idade: 23 },
+    { nome: "Letícia", idade: 55 },
+    { nome: "Eduardo", idade: 19 },
+    { nome: "Rosanna", idade: 32 },
+    { nome: "Walace", idade: 47 }
+];
+
+
+
+
+//const nomes = pessoas.map(pessoa => pessoa.nome);
+//const semNome = pessoas.map(pessoa => ({ idade: pessoa.idade })); // ou delete pessoa.nome
+//const comId = pessoas.map(function (pessoa, index) {
+//    const novaPessoa = { ...pessoa };
+//    novaPessoa.id = index;
+//    return novaPessoa;
+//});
+//console.log(comId);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const pessoasNome5Letras = pessoas.filter(pessoa => pessoa.nome.length >= 5);
+
+//const pessoasMaisDe50 = pessoas.filter(pessoa => pessoa.idade > 50);
+
+//const pessoasNomeTerminaComA = pessoas.filter(pessoa => pessoa.nome.toLowerCase().endsWith("a"));
+
+//console.log(pessoasNome5Letras, pessoasMaisDe50, pessoasNomeTerminaComA);
